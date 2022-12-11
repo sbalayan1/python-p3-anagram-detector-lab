1385ce929ccdaf628d69ef8d7da9f76e7a3cff1a
sbalayan1 <balayans2014@yahoo.com>
2022-12-11 11:05:16 -0800

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, arr_of_words):
+        output = []
+        for word in arr_of_words:
+            if (self.is_anagram(word) == True):
+                output.append(word)
+        
+        return output
+
+    def is_anagram(self, word):
+        sortedA = list(word)
+        sortedB = list(self.word)
+        sortedA.sort()
+        sortedB.sort()
+        if sortedA == sortedB:
+            return True
+        
+        return False
+   
+        
